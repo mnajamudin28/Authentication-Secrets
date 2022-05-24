106dc3b1a3c49a251470473e94a391973b0fd33d
muhamad najamudin <m.najamudin@gmail.com>
2022-05-24 11:01:06 +0700

    change to passport js

--- app.js
+++ app.js
@@ -8,10 +8,7 @@ const session = require('express-session');
 const passport = require ("passport");
 const passportLocalMongoose = require ("passport-local-mongoose");
 
-
 const app = express()
-
-
 
 // console.log(process.env.API_KEY);
 
@@ -19,24 +16,32 @@ app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
 
-app.use(session({
+app.use(session({ //menggunakan express session
     secret: "Our little secret.",
     resave: false,
     saveUninitialized: false
 }))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
 
 const userSchema = new mongoose.Schema({//new mongoose.Schema menambahkan kelas schema
     email: String,
     password: String
 });
 
-//secret kita pindah ke .env
-
-
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy())
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req,res){
     res.render("home"); //diambil dari file home.ejs
@@ -50,26 +55,50 @@ app.get("/register", function (req, res) {
     res.render("register"); //penting untuk form menggunakan method POST
 })
 
+app.get("/secrets", function(req,res){
+    if(req.isAuthenticated()){
+        res.render("secrets");
+    }else{
+        res.redirect("/login");
+    }
+});
+
+app.get("/logout", function(req,res){
+    req.logout();
+    res.redirect("/");
+});
+
 app.post("/register", function(req,res){
 
-    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
-        const newUser = new User({
-            email: req.body.username, //diambil dari body register name username
-            password: hash
-        });
-        newUser.save(function (err) {
-            if (err) {
-                console.log(err);
-            } else {
-                res.render("secrets");
-            }
-        }); 
-    });
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.redirect("/register");
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
     
 });
 
 app.post("/login", function(req,res){
-    
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/secrets");
+            })
+        }
+    })
+
 });
 
 
